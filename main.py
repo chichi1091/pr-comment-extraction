b0cce6114ea473086a9e28b2e749ead0036b27bf
chiharu.terashima <chichi.pa2.1091@gmail.com>
2022-06-24 10:40:42 +0900

    シンタックスハイライトの言語判定

--- main.py
+++ main.py
@@ -13,6 +13,63 @@ PR_REVIEW_COMMENT_TEMPLATE_FILE_NAME = 'pr_review_comment.j2'
 environment = Environment(loader=FileSystemLoader(searchpath=TEMPLATES_DIR_PATH, encoding='utf8'))
 template = environment.get_template(PR_REVIEW_COMMENT_TEMPLATE_FILE_NAME)
 
+def language_decision(fileName):
+    index = fileName.rfind(".")
+    if index <= 0:
+        return ""
+    index = index + 1
+
+    extention = fileName[index:]
+    if extention == "go":
+        return "go"
+    elif extention == "sh":
+        return "shell"
+    elif extention == "c":
+        return "c"
+    elif extention == "cpp":
+        return "cpp"
+    elif extention == "cs":
+        return "csharp"
+    elif extention == "erl":
+        return "erlang"
+    elif extention == "groovy":
+        return "groovy"
+    elif extention == "java":
+        return "java"
+    elif extention == "js":
+        return "javascript"
+    elif extention == "pl":
+        return "perl"
+    elif extention == "php":
+        return "php"
+    elif extention == "py":
+        return "python"
+    elif extention == "rb":
+        return "ruby"
+    elif extention == "ts":
+        return "typescript"
+    elif extention == "vbs":
+        return "vb"
+    elif extention == "scala":
+        return "scala"
+    elif extention == "sql":
+        return "sql"
+    elif extention == "css":
+        return "css"
+    elif extention == "xml":
+        return "xml"
+    elif extention == "m":
+        return "oc"
+    elif extention == "swift":
+        return "swift"
+    elif extention == "kt":
+        return "kotlin"
+    elif extention == "yaml" or extention == "yml":
+        return "yaml"
+    else:
+        return "" 
+
+
 def make_markdown(owner, repository, api, pr):
     comments = api.get_pull_comment(owner, repository, str(pr['number']))
     # asc_comments = sortd(comments, key = lambda i: (i['pull_request_review_id'], i['id']))
@@ -29,6 +86,7 @@ def make_markdown(owner, repository, api, pr):
         json_comment['reviewer'] = comment['user']['login']
         json_comment['file_path'] = comment['path']
         json_comment['diff'] = comment['diff_hunk']
+        json_comment['language'] = language_decision(comment['path'])
         json_comment['comment'] = comment['body']
         json_comments.append(json_comment)
     # print('------------------------------')
